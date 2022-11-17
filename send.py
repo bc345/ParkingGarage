5cb26f8022a3efcf9a218e10ef2ea45fbc615b16
KarrieMN <115417972+KarrieMN@users.noreply.github.com>
2022-11-17 15:43:37 -0500

    Update send.py

--- send.py
+++ send.py
@@ -1,6 +1,6 @@
-import requests 
+import requests             # may have to pip install requests library
 
 url = "https://seniorproject.pythonanywhere.com/uploader"
 
-with open('outputwork.txt', 'rb') as f:
+with open('outputwork.txt', 'rb') as f:                 #change text file name to the name of the output file from the c++ code
     r = requests.post(url, files={'file': f.read()})
