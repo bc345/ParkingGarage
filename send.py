eb94d87e5eb05e7a143b17156578c4583df2e4cf
Brandon Chow <94306950+bc345@users.noreply.github.com>
2022-11-18 12:42:11 -0500

    Update send.py

--- send.py
+++ send.py
@@ -1,6 +1,9 @@
 import requests             # may have to pip install requests library
-
+import time
 url = "https://seniorproject.pythonanywhere.com/uploader"
 
-with open('outputwork.txt', 'rb') as f:                 #change text file name to the name of the output file from the c++ code
-    r = requests.post(url, files={'file': f.read()})
+while True:
+    with open('/home/pi/projects/output.txt', 'rb') as f:                 #change text file name to the name of the output file from the c++ code
+        r = requests.post(url, files={'file': f.read()})
+        print("updated")
+    time.sleep(2)
